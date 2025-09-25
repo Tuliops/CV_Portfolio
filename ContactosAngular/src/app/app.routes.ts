3fee6c76e181ec88ed5b432c8927094b34376eb9
Tuliops <Tuliopirir3@gmail.com>
2025-09-25 01:16:32 -0600

    Delete Ag

--- ContactosAngular/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ContactListComponent } from './components/contact-list/contact-list.component';
-import { ContactFormComponent } from './components/contact-form/contact-form.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'contacts', pathMatch: 'full' },
-  { path: 'contacts', component: ContactListComponent },
-  { path: 'add-contact', component: ContactFormComponent }
-];
